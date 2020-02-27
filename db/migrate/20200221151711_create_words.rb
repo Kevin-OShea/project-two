67a0c2286b720c2520600a019608dfde80d92f32
Kevinoshea <kevinbarretoshea@gmail.com>
2020-02-27 11:59:54 -0500

    Bugfixing

--- db/migrate/20200221151711_create_words.rb
+++ db/migrate/20200221151711_create_words.rb
@@ -1,9 +1,9 @@
 class CreateWords < ActiveRecord::Migration[5.2]
   def change
     create_table :words do |t|
-      t.string :text, null: false, index: { unique: true }
-      t.integer :times_used, null: false, index: { unique: true }
-      t.integer :difficulty, null: false, index: { unique: true }
+      t.string :text, null: false
+      t.integer :times_used, null: false
+      t.integer :difficulty, null: false
 
       t.timestamps
     end
