6dd69484c3f6f653d112f0c6919be9e2659d318e
Kevinoshea <kevinbarretoshea@gmail.com>
2020-02-27 08:41:08 -0500

    undoing last commits

--- db/migrate/20200221151711_create_words.rb
+++ db/migrate/20200221151711_create_words.rb
@@ -2,8 +2,8 @@ class CreateWords < ActiveRecord::Migration[5.2]
   def change
     create_table :words do |t|
       t.string :text, null: false, index: { unique: true }
-      t.integer :times_used, null: false
-      t.integer :difficulty, null: false
+      t.integer :times_used, null: false, index: { unique: true }
+      t.integer :difficulty, null: false, index: { unique: true }
 
       t.timestamps
     end
